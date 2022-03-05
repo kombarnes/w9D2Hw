163fb2ab8a3cc5fffb6cd649c2159a6cadb3b52e
Kourtney <barnes.kourt@gmail.com>
2022-03-05 14:05:51 -0500

    hw

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
 // middleware
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cookieParser());
 
 // view engine
 app.set('view engine', 'ejs');
@@ -21,3 +23,26 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use(authRoutes);
+
+// //create a cookie
+// app.get('/set-cookies', (req, res) => {
+
+//   // res.setHeader('set-cookie', 'newUser-true');
+
+//   //does the same thing as above, looks cleaner
+//   res.cookie('newUser', false);
+//   res.cookie('isEmployee', true, { maxAge: 1000 * 60 * 60 * 24, httpOnly: true });
+
+//   res.send('You got the cookie, check the Application tab in the inspect tool!')
+
+// });
+
+// //read a cookie
+// app.get('/read-cookies', (req, res) => {
+
+//   const cookies = req.cookies;
+//   console.log(cookies);
+
+//   res.json(cookies);
+
+// });
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 // controller actions
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
 
 //handle errors
 const handleErrors = (err) => {
@@ -12,6 +13,7 @@ const handleErrors = (err) => {
     return errors;
   }
 
+  //validation errors
   if (err.message.includes('user validation failed')) {
     Object.values(err.errors).forEach(({ properties }) => {
       errors[properties.path] = properties.message
@@ -25,9 +27,16 @@ const handleErrors = (err) => {
   return errors;
 }
 
+const maxAge = 3 * 24 * 60 * 60;
+const createToken = (id) => {
+  //returns the token
+  return jwt.sign({ id }, 'secret message', {
+    expiresIn: maxAge
+  });
+}
 
 
-
+//controller actions
 module.exports.signup_get = (req, res) => {
   res.render('signup');
 }
@@ -40,17 +49,23 @@ module.exports.signup_post = async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = await User.create({ email, password });
-    res.status(201).json(user)
+    const token = createToken(user._id)
+    res.cookie('jwt', token, {httpOnly: true, maxAge: maxAge * 1000});
+    res.status(201).json({user: user._id})
   }
   catch (err) {
-    const errors = handleErrors(err)
+    const errors = handleErrors(err);
     res.status(400).json({ errors });
   }
 }
 
 module.exports.login_post = async (req, res) => {
   const { email, password } = req.body;
-
-  console.log(email, password);
-  res.send('user login');
+  try {
+    const user = await User.login(email, password);
+    res.status(200).json({ user: user._id });
+  }
+  catch (err) {
+    res.status(400).json({});
+  }
 }
